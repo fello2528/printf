ff60e07deb02849e3b52ba462323dd108a031bb9
fello2528 <felixmwangimukoma@gmail.com>
2022-10-20 08:37:13 -0700

    printf

---
+++ _printf.c
@@ -0,0 +1,48 @@
+#include "main.h"
+/**
+ * _printf - replicate prinft
+ * @format: format printf
+ * Return: Always 0
+ */
+int _printf(const char *format, ...)
+{
+
+	int (*printer)(va_list);
+	int length = 0;
+	va_list content;
+
+	va_start(content, format);
+
+	if (!format || (format[0] == '%' && !format[1]))
+		return (-1);
+
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
+
+	for (; *format; format++)
+	{
+		if (*format == '%')
+		{
+			format++;
+
+			printer = get_print(*format);
+
+			if (printer)
+			{
+				length += printer(content);
+			}
+			else
+			{
+				length += _putchar('%');
+				length += _putchar(*format);
+			}
+		}
+		else
+		{
+			length += _putchar(*format);
+		}
+	}
+	va_end(content);
+
+	return (length);
+}
